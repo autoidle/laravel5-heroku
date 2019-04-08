d52952f946c3565a97d7c73fc30050a131638734
Stefan Gasser <stefangasser.info@gmail.com>
2019-04-08 23:03:45 +0200

    Force https if not local or testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!App::environment(['local', 'testing'])) {
+            URL::forceScheme('https');
+        }
     }
 }
